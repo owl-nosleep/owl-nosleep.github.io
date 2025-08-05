cfe20018fc213d5f7e69e3418c5fe2910ad7060e
matt930415 <matt930415@gmail.com>
2025-08-05 17:14:47 +0800

    Article altered.

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
+			name: "Credit",
 			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
