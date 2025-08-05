b5cbd259c99e8393719ddfb19192e03cff890444
matt930415 <matt930415@gmail.com>
2025-08-05 20:49:54 +0800

    Order altered.

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -77,7 +77,7 @@ export const experiences: Experience[] = [
 		year: "2025",
 		title: "成大資安社",
 		description: "副社長",
-		type: "achievement",
+		type: "education",
 		order: 4,
 		content: "擔任成大資安社副社長，負責組織各種資安活動和分享會，促進校園資安知識交流。"
 	},
